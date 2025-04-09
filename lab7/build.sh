8ddeadb2b5b64d8f26b8ac1cad66f1fe73d15874
petru-braha <petrubraha@gmail.com>
2025-04-09 13:42:24 +0300

    lab7;

--- lab7/build.sh
+++ lab7/build.sh
@@ -1,5 +1,15 @@
 #!/bim.bash
 
-sudo cp index.html /opt/lampp/htdocs/petru/
-sudo cp login.php /opt/lampp/htdocs/petru/
-sudo cp logout.php /opt/lampp/htdocs/petru/
+path=""
+if [ "$0" = "./test.sh" ]; then
+  path=""
+elif [ "$0" = "./lab7/test.sh" ]; then
+  path="lab7/"
+else 
+  echo "error: invalid root path"
+  exit 1
+fi
+
+# TODO : write prefix
+cp index.html login.php logout.php tables.sql /opt/lampp/htdocs/petru/
+# TODO MAKE DATABASE XAMPP WORK, MAKE LOGIN LOGOUT PAGES FAST
