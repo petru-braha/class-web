3de7fc6da4450bf7bbff9493da13ac8f753bb86d
p <petrubraha@gmail.com>
2025-05-05 00:25:59 +0300

    changed file encoding; NOW XAMPP UNDERSTANDS DUAAHHH;

--- lab9/post_view.php
+++ lab9/post_view.php
@@ -1,18 +1,1 @@
 <?php
-
-echo '<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title></title>
-    <meta name="description" content="" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="small.css" />
-  </head>
-  <body>
-    <main>';
-
-echo '
-  </main>
-  </body>
-  </html>';
