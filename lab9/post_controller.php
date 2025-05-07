90d7a22a4f251c51b52b541a23ffa7159f23362f
P <petrubraha@gmail.com>
2025-05-07 12:47:02 +0300

    changed the controller

--- lab9/post_controller.php
+++ lab9/post_controller.php
@@ -1,7 +1,5 @@
 <?php
 
 include_once "post_model.php";
-include_once "post_view.php";
-
 $posts = getPosts();
-renderPosts($posts);
+include_once "post_view.php";
--- lab9/post_view.php
+++ lab9/post_view.php
@@ -1,29 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-function renderPosts($posts): void
-{
-  echo '
-    <html lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <title>lab9-posts</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="small.css" />
-      </head>
-      <body>
-        <main>';
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>lab9-posts</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="small.css" />
+</head>
 
-  foreach ($posts as $post) {
-    echo '<article>';
-    echo '<h2>' . htmlspecialchars($post['username']) . ':</h2>';
-    echo '<p>' . htmlspecialchars($post['description']) . '</p>';
-    echo '<img src="' . htmlspecialchars($post['imageUrl']) . '" alt="Post image" style="height: 300px;">';
-    echo '</article>';
-  }
+<body>
+  <main>
 
-  echo '
-        </main>
-      </body>
-    </html>';
-}
+    <?php
+    foreach ($posts as $post) {
+      echo '<article>';
+      echo '<h2>' . htmlspecialchars($post['username']) . ':</h2>';
+      echo '<p>' . htmlspecialchars($post['description']) . '</p>';
+      echo '<img src="' . htmlspecialchars($post['imageUrl']) . '" alt="Post image" style="height: 300px;">';
+      echo '</article>';
+    }
+    ?>
+
+  </main>
+</body>
+
+</html>
